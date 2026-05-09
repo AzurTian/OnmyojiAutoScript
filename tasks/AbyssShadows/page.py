977c18d4d8cb22006ff03f3c4e8fba1e6fd79f13
magicemptycity <2214463059@qq.com>
2026-05-09 23:40:34 +0800

    fix(AbyssShadows): 修复连接式神记录页面的成本参数

--- tasks/AbyssShadows/page.py
+++ tasks/AbyssShadows/page.py
@@ -5,5 +5,5 @@ from tasks.GlobalGame.assets import GlobalGameAssets
 page_abyss = Page(AbyssShadowsAssets.I_CHECK_ABYSS)
 page_shirin.connect(page_abyss, AbyssShadowsAssets.L_SHENSHE_TO_ABYSS, key="page_shirin->page_abyss")
 page_abyss.connect(page_shirin, GlobalGameAssets.I_UI_BACK_YELLOW, key="page_abyss->page_shirin")
-page_abyss.connect(page_shikigami_records, AbyssShadowsAssets.I_ABYSS_SHIKI, key="page_abyss->page_shikigami_records")
-page_shikigami_records.connect(page_abyss, GlobalGameAssets.I_UI_BACK_YELLOW, key="page_shikigami_records->page_abyss")
+page_abyss.connect(page_shikigami_records, AbyssShadowsAssets.I_ABYSS_SHIKI, key="page_abyss->page_shikigami_records", cost=4)
+page_shikigami_records.connect(page_abyss, GlobalGameAssets.I_UI_BACK_YELLOW, key="page_shikigami_records->page_abyss", cost=4)
