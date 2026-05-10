07b9549d5ff66053ff0b3ddcb1ab38b9d6c67bdb
magicemptycity <2214463059@qq.com>
2026-05-11 04:57:51 +0800

    feat(AbyssShadows): 添加新的页面连接至主界面并更新相关资源

--- tasks/AbyssShadows/page.py
+++ tasks/AbyssShadows/page.py
@@ -1,6 +1,6 @@
 from tasks.AbyssShadows.assets import AbyssShadowsAssets
 from tasks.GameUi.assets import GameUiAssets
-from tasks.GameUi.page import Page, page_shirin, page_shikigami_records
+from tasks.GameUi.page import Page, page_shirin, page_shikigami_records, page_main
 from tasks.GlobalGame.assets import GlobalGameAssets
 
 page_abyss = Page(AbyssShadowsAssets.I_CHECK_ABYSS)
@@ -9,5 +9,6 @@ page_abyss_shikigami_records = Page(GameUiAssets.I_CHECK_RECORDS, category="abys
 page_shirin.connect(page_abyss, AbyssShadowsAssets.L_SHENSHE_TO_ABYSS, key="page_shirin->page_abyss")
 page_abyss.connect(page_shirin, GlobalGameAssets.I_UI_BACK_YELLOW, key="page_abyss->page_shirin")
 page_abyss_map.connect(page_abyss, GlobalGameAssets.I_UI_BACK_YELLOW, key="page_abyss_map->page_abyss")
+page_abyss_map.connect(page_main, GlobalGameAssets.I_UI_ANY_TO_MAIN, key="page_abyss_map->page_main")
 page_abyss_map.connect(page_abyss_shikigami_records, AbyssShadowsAssets.I_ABYSS_SHIKI, key="page_abyss_map->page_abyss_shikigami_records", cost=4)
 page_abyss_shikigami_records.connect(page_abyss_map, GlobalGameAssets.I_UI_BACK_YELLOW, key="page_abyss_shikigami_records->page_abyss_map", cost=4)
